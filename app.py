383df989fc2926e4e853b500f15006977a731e62
Duncan Wang <wxlunn@gmail.com>
2023-10-16 18:39:12 +0800

    db connect init

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+import pymongo
+
+client=pymongo.MongoClient("mongodb+srv://root:root0320@trainingcluster.ymqgddi.mongodb.net/?retryWrites=true&w=majority")
+
+app = Flask(__name__,
+            static_folder="public",
+            static_url_path="/")
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return "Hello"
 
+app.secret_key="any string"
 app.run()
