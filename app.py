ac2abb369afc17966d7772b900ba1dc4295f8553
Duncan Wang <wxlunn@gmail.com>
2023-10-17 14:08:48 +0800

    completed

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
 from flask import *
 import pymongo
 
-client=pymongo.MongoClient("")
+# mongodb init
+client=pymongo.MongoClient("mongodb url")
 db=client.member_system
 print("db connect successfully")
 
+# server init
 app = Flask(__name__,
             static_folder="public",
             static_url_path="/")
 
+app.secret_key="any string"
+
+# homepage
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# member page
 @app.route("/member")
 def memeber():
-    return render_template("member.html")
+    if "nickname" in session:
+        return render_template("member.html")
+    else:
+        return redirect("/")
 
+# error page
 @app.route("/error")
 def error():
     msg = request.args.get("msg", "error occur, please contact us")
     return render_template("error.html", msg=msg)
 
-@app.route("/signup", method=["POST"])
+# handle signup func
+@app.route("/signup", methods=["POST"])
 def signup():
-    # receive data from frontend 
+    # receive data from frontend signup
     nickname = request.form["nickname"]
     email = request.form["email"]
     password = request.form["password"]
@@ -45,6 +56,27 @@ def signup():
 
     return redirect('/')
 
+# handle signin func
+@app.route("/signin", methods=["POST"])
+def signin():
+    email = request.form["email"]
+    password = request.form["password"]
+    collection = db.user
+    result=collection.find_one({
+        "$and":[
+        {"email":email},
+        {"password":password}
+        ]
+    })
+    if result == None:
+        return redirect("/error?meg=Email or Password is wrong")
+    session["nickname"] = result["nickname"]
+    return redirect("/member")
 
-app.secret_key="any string"
+# handle signout dunc
+@app.route("/signout")
+def signout():
+    del session["nickname"]
+    return redirect("/")
+
 app.run()
