35c655b045e099bb971443107600ef8badaa84b2
Duncan Wang <wxlunn@gmail.com>
2023-10-17 11:09:51 +0800

    add frontend page

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import *
 import pymongo
 
-client=pymongo.MongoClient("...")
+client=pymongo.MongoClient("")
+db=client.member_system
+print("db connect successfully")
 
 app = Flask(__name__,
             static_folder="public",
             static_url_path="/")
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "Hello"
+    return render_template("index.html")
+
+@app.route("/member")
+def memeber():
+    return render_template("member.html")
+
+@app.route("/error")
+def error():
+    msg = request.args.get("msg", "error occur, please contact us")
+    return render_template("error.html", msg=msg)
 
 app.secret_key="any string"
 app.run()
